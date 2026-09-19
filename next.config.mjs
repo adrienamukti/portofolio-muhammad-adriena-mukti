a568fa016f244c7cf77a6b84392091ea84c0565a
Adriena Mukti <muhammadadriena08@gmail.com>
2026-09-19 17:20:01 +0700

    configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+  output: 'export',
+  basePath: '/portofolio-muhammad-adriena-mukti',
+  assetPrefix: '/portofolio-muhammad-adriena-mukti',
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
